e18e6226fa81aa3e898a5fc7e933dc600de4f4e6
Thomas Lovén <thomasloven@gmail.com>
2021-02-26 12:30:02 +0000

    Make work with card-mod 3.0.0

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { LitElement, html, css } from "card-tools/src/lit-element.js";
 import { hass } from "card-tools/src/hass.js";
-import { DOMAINS_HIDE_MORE_INFO, createEntityRow } from "card-tools/src/lovelace-element.js";
+import {
+  DOMAINS_HIDE_MORE_INFO,
+  createEntityRow,
+} from "card-tools/src/lovelace-element.js";
 
 class FoldEntityRow extends LitElement {
   static get properties() {
@@ -21,118 +24,105 @@ class FoldEntityRow extends LitElement {
     this.open = this.open || this._config.open;
 
     let head = this._config.head;
-    if (this._config.entity)
-      head = this._config.entity;
-    if(!head) {
+    if (this._config.entity) head = this._config.entity;
+    if (!head) {
       throw new Error("No fold head specified");
     }
-    if (typeof head === "string")
-      head = {entity: head};
+    if (typeof head === "string") head = { entity: head };
 
     // Items are taken from the first available of the following
     // - The group specified as head
     // - config entities: (this allows auto-population of the list)
     // - config items: (for backwards compatibility - not recommended)
     let items = this._config.items;
-    if (this._config.entities !== undefined)
-      items = this._config.entities;
+    if (this._config.entities !== undefined) items = this._config.entities;
     if (head.entity && head.entity.startsWith("group.") && items === undefined)
       items = hass().states[head.entity].attributes.entity_id;
-    else
-      if(items === undefined)
-        throw new Error("No entities specified.");
-      else if(!items || items.length === undefined)
-        throw new Error("Entities must be a list.");
+    else if (items === undefined) throw new Error("No entities specified.");
+    else if (!items || items.length === undefined)
+      throw new Error("Entities must be a list.");
 
     const fix_config = (config) => {
-      if(typeof config === "string")
-        config = {entity: config};
+      if (typeof config === "string") config = { entity: config };
       return Object.assign({}, this._config.group_config, config);
-    }
+    };
 
     this.head = createEntityRow(head);
     this.head.hass = hass();
     this.head.addEventListener("click", (ev) => {
-      if(!this.hasMoreInfo(head) && !head.tap_action)
-      this.toggle(ev);
+      if (!this.hasMoreInfo(head) && !head.tap_action) this.toggle(ev);
     });
-    this.head.setAttribute('head', 'head');
+    this.head.setAttribute("head", "head");
     this.applyStyle(this.head, head);
-    if(this.head.tagName === "HUI-SECTION-ROW") {
+    if (this.head.tagName === "HUI-SECTION-ROW") {
       customElements.whenDefined(this.head.localName).then(async () => {
         await this.updateComplete;
         await this.head.updateComplete;
-          this.head.shadowRoot.querySelector(".divider").style.marginRight = "-56px";
+        this.head.shadowRoot.querySelector(".divider").style.marginRight =
+          "-56px";
       });
     }
 
     this.rows = items.map((i) => {
       const row = createEntityRow(fix_config(i));
       row.hass = hass();
-      if(this.hasMoreInfo(i))
-        row.classList.add("state-card-dialog");
+      if (this.hasMoreInfo(i)) row.classList.add("state-card-dialog");
       this.applyStyle(row, fix_config(i));
       return row;
     });
   }
 
   async applyStyle(root, config) {
-    if(!config.style) return;
-
-    //await customElements.whenDefined("card-mod");
-    if(root.updateComplete)
-      await root.updateComplete;
-
-    const cm = document.createElement("card-mod");
-    cm.template = {
-      template: config.style,
-      variables: {config},
-      entity_ids: config.entity_ids,
-    };
-    customElements.whenDefined(root.localName).then(() => {
-      if(root.shadowRoot)
-        root.shadowRoot.appendChild(cm);
-    });
+    await customElements.whenDefined("card-mod");
+    customElements
+      .get("card-mod")
+      .applyToElement(
+        root,
+        "row",
+        config.card_mod ? config.card_mod.style : config.style,
+        {
+          config,
+        }
+      );
   }
 
   toggle(ev) {
-    if(ev)
-      ev.stopPropagation();
+    if (ev) ev.stopPropagation();
     this.open = !this.open;
   }
 
   hasMoreInfo(config) {
-    const entity = config.entity || (typeof config === "string" ? config : null);
-    if(entity && !DOMAINS_HIDE_MORE_INFO.includes(entity.split(".",1)[0]))
+    const entity =
+      config.entity || (typeof config === "string" ? config : null);
+    if (entity && !DOMAINS_HIDE_MORE_INFO.includes(entity.split(".", 1)[0]))
       return true;
     return false;
   }
 
   set hass(hass) {
-    this.rows.forEach((e) => e.hass = hass);
+    this.rows.forEach((e) => (e.hass = hass));
     this.head.hass = hass;
   }
 
   render() {
     return html`
-    <div id="head" ?open=${this.open}>
-      ${this.head}
-      <ha-icon
-        @click=${this.toggle}
-        icon=${this.open ? "mdi:chevron-up" : "mdi:chevron-down"}
-      ></ha-icon>
-    </div>
+      <div id="head" ?open=${this.open}>
+        ${this.head}
+        <ha-icon
+          @click=${this.toggle}
+          icon=${this.open ? "mdi:chevron-up" : "mdi:chevron-down"}
+        ></ha-icon>
+      </div>
 
-    <div id="items"
-      ?open=${this.open}
-      style=
-        ${this._config.padding
+      <div
+        id="items"
+        ?open=${this.open}
+        style=${this._config.padding
           ? `padding-left: ${this._config.padding}px;`
-          : ''
-        }
-    >
-      ${this.rows}
-    </div>
+          : ""}
+      >
+        ${this.rows}
+      </div>
     `;
   }
 
@@ -150,7 +140,7 @@ class FoldEntityRow extends LitElement {
       }
       #head ha-icon {
         width: var(--toggle-icon-width);
-        cursor: pointer
+        cursor: pointer;
       }
 
       #items {
@@ -168,14 +158,14 @@ class FoldEntityRow extends LitElement {
       }
     `;
   }
-
 }
 
-
-if(!customElements.get("fold-entity-row")) {
-  customElements.define('fold-entity-row', FoldEntityRow);
-  const pjson = require('../package.json');
-  console.info(`%cFOLD-ENTITY-ROW ${pjson.version} IS INSTALLED`,
-  "color: green; font-weight: bold",
-  "");
+if (!customElements.get("fold-entity-row")) {
+  customElements.define("fold-entity-row", FoldEntityRow);
+  const pjson = require("../package.json");
+  console.info(
+    `%cFOLD-ENTITY-ROW ${pjson.version} IS INSTALLED`,
+    "color: green; font-weight: bold",
+    ""
+  );
 }
